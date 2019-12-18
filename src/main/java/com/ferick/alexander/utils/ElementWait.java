075732ecc110159bb9750a80ba9b0f9f53fa1119
Cuchillon <ferickadmin@gmail.com>
2019-12-18 19:14:28 +0300

    Adding wait timeouts

--- src/main/java/com/ferick/alexander/utils/ElementWait.java
+++ src/main/java/com/ferick/alexander/utils/ElementWait.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class ElementWait {
 
     private static final long DEFAULT_TIMEOUT = 10;
@@ -11,6 +13,16 @@ public class ElementWait {
 
     public ElementWait(WebDriver driver) {
         wait = new WebDriverWait(driver, DEFAULT_TIMEOUT);
+    }
+
+    public ElementWait withTimeout(long timeOutInSeconds) {
+        wait.withTimeout(Duration.ofSeconds(timeOutInSeconds));
+        return this;
+    }
+
+    public ElementWait pollingEvery(long sleepInMillis) {
+        wait.pollingEvery(Duration.ofMillis(sleepInMillis));
+        return this;
     }
 
     public boolean isElementDisplayed(WebElement element) {
