95a0e811940ded17a31fd500efe65aace5ecb40e
Cuchillon <ferickadmin@gmail.com>
2019-12-31 21:29:02 +0300

    Optimize imports

--- src/main/java/com/ferick/alexander/elements/ElementFieldDecorator.java
+++ src/main/java/com/ferick/alexander/elements/ElementFieldDecorator.java
@@ -15,7 +15,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ElementFieldDecorator extends DefaultFieldDecorator {
